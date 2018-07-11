70857aa684404ea627c5e7d12e0bb1c52053e667
Anny Patel <30823493+annypatel@users.noreply.github.com>
2018-07-11 14:13:35 +0530

    Add dagger module for schedulers for integration tests

---
+++ base/test_ui/src/main/java/tickmarks/ui/test/rx/TestSchedulersModule.kt
@@ -0,0 +1,50 @@
+package tickmarks.ui.test.rx
+
+import androidx.test.espresso.IdlingRegistry
+import com.squareup.rx2.idler.Rx2Idler
+import dagger.Module
+import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import tickmarks.data.rx.DataSchedulers
+import tickmarks.domain.rx.DomainSchedulers
+import tickmarks.ui.rx.UiSchedulers
+import javax.inject.Singleton
+
+/**
+ * Dagger [module][Module] for schedulers that declares [UiSchedulers], [DomainSchedulers] and [DataSchedulers] to be
+ * bind into application level object graph for integration tests.
+ */
+@Module
+object TestSchedulersModule {
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun uiSchedulers() = UiSchedulers(
+        mainThread = wrap(AndroidSchedulers.mainThread())
+    )
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun domainSchedulers() = DomainSchedulers(
+        computation = wrap(Schedulers.computation())
+    )
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun dataSchedulers() = DataSchedulers(
+        io = wrap(Schedulers.io()),
+        database = wrap(Schedulers.single())
+    )
+
+    @JvmStatic
+    private fun wrap(scheduler: Scheduler): Scheduler {
+        val wrapped = Rx2Idler.wrap(scheduler, "")
+        IdlingRegistry.getInstance().register(wrapped)
+        return wrapped
+    }
+}
