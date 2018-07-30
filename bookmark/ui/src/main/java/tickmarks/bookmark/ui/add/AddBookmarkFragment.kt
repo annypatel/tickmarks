c110ec0c21e25eaef74fb2d72ca4177df14f05b2
Anny Patel <30823493+annypatel@users.noreply.github.com>
2018-07-30 11:18:14 +0530

    Remove some boilerplate from AddBookmarkFragment

--- bookmark/ui/src/main/java/tickmarks/bookmark/ui/add/AddBookmarkFragment.kt
+++ bookmark/ui/src/main/java/tickmarks/bookmark/ui/add/AddBookmarkFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import dagger.android.support.AndroidSupportInjection
-import tickmarks.bookmark.ui.add.AddBookmarkFragmentBinding.inflate
 import tickmarks.ui.fragment.viewModel
 import javax.inject.Inject
 
@@ -18,7 +17,6 @@ class AddBookmarkFragment : Fragment() {
 
     @Inject
     lateinit var factory: ViewModelProvider.Factory
-    private lateinit var binding: AddBookmarkFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidSupportInjection.inject(this)
@@ -26,14 +24,8 @@ class AddBookmarkFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = inflate(inflater, container, false)
+        val binding = AddBookmarkFragmentBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel(factory)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val viewModel = viewModel<AddBookmarkViewModel>(factory)
-        binding.viewModel = viewModel
     }
 }
