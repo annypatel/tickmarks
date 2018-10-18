529b37ab63c2a90a74c90b67de7687af8c3da4fa
Anny Patel <30823493+annypatel@users.noreply.github.com>
2018-10-18 17:03:08 +0530

    Bump-up room version to 2.1.0-alpha01 which supports various rx return types for query methods

--- bookmark/data/src/main/java/tickmarks/bookmark/data/database/BookmarkDao.kt
+++ bookmark/data/src/main/java/tickmarks/bookmark/data/database/BookmarkDao.kt
@@ -4,30 +4,25 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import io.reactivex.Completable
-import io.reactivex.Completable.fromAction
 import io.reactivex.Single
 
 /**
  * Data Access Object for the bookmarks table.
  */
 @Dao
-abstract class BookmarkDao {
+interface BookmarkDao {
 
     /**
      * Inserts a bookmark in the database.
      *
      * @param bookmark The bookmark to be inserted.
      */
-    open fun insert(bookmark: BookmarkEntity): Completable = fromAction {
-        insertImpl(bookmark)
-    }
-
     @Insert
-    protected abstract fun insertImpl(bookmark: BookmarkEntity)
+    fun insert(bookmark: BookmarkEntity): Completable
 
     /**
      * To get all bookmarks.
      */
     @Query("SELECT * FROM bookmarks")
-    abstract fun getAll(): Single<List<BookmarkEntity>>
+    fun getAll(): Single<List<BookmarkEntity>>
 }
