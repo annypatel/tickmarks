f00ee3e39653d4b18a7b00dae76df39c6eee047a
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-01-30 15:32:48 +0530

    Add test for Snackbar binding adapter

---
+++ base/ui/src/androidTest/java/tickmarks/ui/binding/SnackbarBindingAdapterTest.kt
@@ -0,0 +1,49 @@
+package tickmarks.ui.binding
+
+import android.view.ContextThemeWrapper
+import android.view.View
+import android.widget.FrameLayout
+import androidx.test.annotation.UiThreadTest
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import org.junit.Before
+import org.junit.Test
+import tickmarks.ui.R
+import tickmarks.ui.event.Event
+import tickmarks.ui.event.consume
+import tickmarks.ui.test.assertThat
+import tickmarks.ui.test.hasNoSnackbar
+import tickmarks.ui.test.hasSnackbar
+import tickmarks.ui.test.withMessage
+
+class SnackbarBindingAdapterTest {
+
+    private lateinit var container: View
+
+    @Before
+    fun setUp() {
+        val context = ContextThemeWrapper(getInstrumentation().targetContext, R.style.Theme_AppCompat)
+        container = FrameLayout(context)
+    }
+
+    @Test
+    @UiThreadTest
+    fun showSnackbar_givenEvent_shouldShowSnackbar() {
+        val snackbarMsg = android.R.string.ok
+
+        SnackbarBindingAdapter.showSnackbar(container, Event(snackbarMsg))
+
+        assertThat(container, hasSnackbar(withMessage(snackbarMsg)))
+    }
+
+    @Test
+    @UiThreadTest
+    fun showSnackbar_givenConsumedEvent_shouldNotShowSnackbar() {
+        val snackbarMsg = android.R.string.ok
+        val event = Event(snackbarMsg)
+        event.consume {  }
+
+        SnackbarBindingAdapter.showSnackbar(container, event)
+
+        assertThat(container, hasNoSnackbar())
+    }
+}
