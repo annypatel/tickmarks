f74b30ee2f5f67f2377023807a39a9340129c000
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-03-04 13:07:36 +0530

    Migrate SnackbarBindingAdapter instrumented tests to unit tests #4

--- base/ui/src/test/java/tickmarks/ui/binding/SnackbarBindingAdapterTest.kt
+++ base/ui/src/test/java/tickmarks/ui/binding/SnackbarBindingAdapterTest.kt
@@ -3,30 +3,31 @@ package tickmarks.ui.binding
 import android.view.ContextThemeWrapper
 import android.view.View
 import android.widget.FrameLayout
-import androidx.test.annotation.UiThreadTest
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import tickmarks.ui.R
 import tickmarks.ui.event.Event
 import tickmarks.ui.event.consume
-import tickmarks.ui.test.assertThat
 import tickmarks.ui.test.hasNoSnackbar
 import tickmarks.ui.test.hasSnackbar
 import tickmarks.ui.test.withMessage
 
+@RunWith(AndroidJUnit4::class)
 class SnackbarBindingAdapterTest {
 
     private lateinit var container: View
 
     @Before
     fun setUp() {
-        val context = ContextThemeWrapper(getInstrumentation().targetContext, R.style.Theme_AppCompat)
+        val context = ContextThemeWrapper(getApplicationContext(), R.style.Theme_AppCompat)
         container = FrameLayout(context)
     }
 
     @Test
-    @UiThreadTest
     fun showSnackbar_givenEvent_shouldShowSnackbar() {
         val snackbarMsg = android.R.string.ok
 
@@ -36,7 +37,6 @@ class SnackbarBindingAdapterTest {
     }
 
     @Test
-    @UiThreadTest
     fun showSnackbar_givenConsumedEvent_shouldNotShowSnackbar() {
         val snackbarMsg = android.R.string.ok
         val event = Event(snackbarMsg)
@@ -48,7 +48,6 @@ class SnackbarBindingAdapterTest {
     }
 
     @Test
-    @UiThreadTest
     fun showSnackbar_givenNull_shouldNotShowSnackbar() {
         container.showSnackbar(null)
 
