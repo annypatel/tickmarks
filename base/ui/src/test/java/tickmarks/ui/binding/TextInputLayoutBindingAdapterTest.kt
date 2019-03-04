282ef3b66e79d431633fc562c350f6050ff1f15b
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-03-04 13:26:48 +0530

    Using robolectric to test TextInputLayout binding adapter #4

--- base/ui/src/test/java/tickmarks/ui/binding/TextInputLayoutBindingAdapterTest.kt
+++ base/ui/src/test/java/tickmarks/ui/binding/TextInputLayoutBindingAdapterTest.kt
@@ -1,42 +1,41 @@
 package tickmarks.ui.binding
 
-import android.content.Context
+import android.view.ContextThemeWrapper
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.textfield.TextInputLayout
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
+import org.hamcrest.CoreMatchers.nullValue
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import org.junit.runner.RunWith
+import tickmarks.ui.R
+import tickmarks.ui.test.equalToRes
 
+@RunWith(AndroidJUnit4::class)
 class TextInputLayoutBindingAdapterTest {
 
-    @Mock
     private lateinit var til: TextInputLayout
 
     @Before
     fun setup() {
-        MockitoAnnotations.initMocks(this)
+        val context = ContextThemeWrapper(getApplicationContext(), R.style.Theme_AppCompat)
+        til = TextInputLayout(context)
     }
 
     @Test
     fun setError_givenNone_setErrorToNull() {
         til.setError(NONE)
 
-        verify(til).error = null
+        assertThat(til.error, nullValue())
     }
 
     @Test
     fun setError_givenStringRes_setErrorToStringValue() {
-        val stringRes = 123
-        val expectedStringValue = "Hello, world!"
-        val context = mock<Context>()
-        whenever(context.getString(stringRes)).thenReturn(expectedStringValue)
-        whenever(til.context).thenReturn(context)
+        val stringRes = android.R.string.ok
 
         til.setError(stringRes)
 
-        verify(til).error = expectedStringValue
+        assertThat(til.error, equalToRes(stringRes))
     }
 }
--- test-utils/ui/src/main/java/tickmarks/ui/test/ResourceMatchers.kt
+++ test-utils/ui/src/main/java/tickmarks/ui/test/ResourceMatchers.kt
@@ -1,7 +1,8 @@
 package tickmarks.ui.test
 
+import android.content.Context
 import androidx.annotation.StringRes
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.Matcher
 
@@ -9,6 +10,6 @@ import org.hamcrest.Matcher
  * Creates a matcher that matches string with string resource id.
  */
 fun equalToRes(@StringRes resId: Int): Matcher<CharSequence?> {
-    val string = getInstrumentation().targetContext.getString(resId)
+    val string = getApplicationContext<Context>().getString(resId)
     return equalTo(string)
 }
