22fe9b2c2a74037b87daea939959b1d5d524bc4e
Anny Patel <30823493+annypatel@users.noreply.github.com>
2020-09-28 21:44:26 +0200

    Configure android git version plugin (#11)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 repositories {
     google()
     mavenCentral()
+    gradlePluginPortal()
     jcenter()
 }
 
@@ -13,4 +14,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10") // Upgrading kotlin plugin? Update kotlin version in Dependencies.kt as well.
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.13.1") // Upgrading detekt plugin? Update detekt version in Dependencies.kt as well.
     implementation("com.github.ben-manes:gradle-versions-plugin:0.33.0")
+    implementation("com.gladed.androidgitversion:gradle-android-git-version:0.4.13")
 }
