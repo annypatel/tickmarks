8427eaaba3831a86947ca4dab4bbe62491bec1c6
Anny Patel <30823493+annypatel@users.noreply.github.com>
2020-10-05 14:49:52 +0200

    Fix env variable format and always run hide secret

--- .circleci/reveal_secrets.sh
+++ .circleci/reveal_secrets.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# install git-secret
 sudo apt-get update && sudo apt-get install apt-transport-https
 echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
 wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
 sudo apt-get update && sudo apt-get install git-secret
 
-echo -e "$GPG_KEY" | gpg --import
+# import GPG key
+echo $GPG_KEY | gpg --import
 
+# reveal secret
 git secret reveal -f
