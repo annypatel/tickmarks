dedcf5bedcb3ae4745aa45a48abde009195e8e52
Anny Patel <30823493+annypatel@users.noreply.github.com>
2020-10-05 19:55:47 +0200

    Deploy APK on firebase (#16)
    * Add firebase config for deployment
    
    * Add google service json file
    
    * Add deploy step on CI
    
    * Rename workflow for ci
    
    * Add test group for distribution and run deploy only for master branch

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,9 +11,13 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:4.0.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10") // Upgrading kotlin plugin? Update kotlin version in Dependencies.kt as well.
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.13.1") // Upgrading detekt plugin? Update detekt version in Dependencies.kt as well.
+    // Upgrading kotlin plugin? Update kotlin version in Dependencies.kt as well.
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+    // Upgrading detekt plugin? Update detekt version in Dependencies.kt as well.
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.13.1")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.33.0")
     implementation("com.gladed.androidgitversion:gradle-android-git-version:0.4.13")
     implementation("com.osacky.flank.gradle:fladle:0.12.1")
+    implementation("com.google.gms:google-services:4.3.4")
+    implementation("com.google.firebase:firebase-appdistribution-gradle:2.0.1")
 }
