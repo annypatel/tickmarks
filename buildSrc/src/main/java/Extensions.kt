c87108e2348e038695e8c48564f1826a473eabb6
Anny Patel <30823493+annypatel@users.noreply.github.com>
2020-10-09 15:06:02 +0200

    Deploy to firebase with release notes (#21)
    * Add new gradle task which depends on app distribution upload task

--- buildSrc/src/main/java/Extensions.kt
+++ buildSrc/src/main/java/Extensions.kt
@@ -1,8 +1,13 @@
 import org.gradle.api.Project
+import java.io.File
 
 var Project.archivesBaseName
     get() = property("archivesBaseName")
     set(value) = setProperty("archivesBaseName", value)
 
-val Project.ci
+val ci
     get() = System.getenv("CI") == "true"
+
+fun Project.buildDir(file: String) = File(buildDir, file)
+
+fun Project.relativeToRoot() = projectDir.toRelativeString(rootProject.projectDir)
