50ec36920c8d3040ca737be1d67211d754f1e899
Anny Patel <30823493+annypatel@users.noreply.github.com>
2021-05-21 19:27:02 +0200

    Fix git-secret for ci (#30)

--- .circleci/reveal_secrets.sh
+++ .circleci/reveal_secrets.sh
@@ -2,9 +2,9 @@
 
 # install git-secret
 sudo apt-get update && sudo apt-get install apt-transport-https
-echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | sudo tee -a /etc/apt/sources.list
-wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | sudo apt-key add -
-sudo apt-get update && sudo apt-get install git-secret
+sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
+wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y git-secret
 
 # import GPG key
 echo $GPG_KEY | gpg --import --quiet
